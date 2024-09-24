b11a9e3e58ac2ae30a87fde7cc529ed472aa6f2e
Ivar Arnesson <ivar.arnesson@gmail.com>
2024-09-24 15:00:55 +0200

    Added Navbar components and assets

--- BudgetPlanner.Web/src/App.jsx
+++ BudgetPlanner.Web/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import TodoList from './TodoList'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CustomNavbar from './components/CustomNavbar'
 
 function App() {
   
   return (
-      <TodoList />
+      <CustomNavbar />
   )
 }
 
---
+++ BudgetPlanner.Web/src/components/CustomNavbar.jsx
@@ -0,0 +1,55 @@
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from 'react-router-dom';
+import "./CustomNavbar.css"
+import bpIcon from "../assets/BP-icon.png"
+
+export default function CustomNavbar() {
+  return (
+    <Navbar expand="lg" className="nav">
+      <Container fluid>
+        <Navbar.Brand className="nav--title" href="#">
+          <img
+            className="nav--img"
+            src={bpIcon}
+            alt='Brand'
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#annannnComponent">Dashboard</Nav.Link>
+            <Nav.Link href="#annannComponent">Income</Nav.Link>
+            <Nav.Link href="#annannnnnComponent">Savings</Nav.Link>
+            <Nav.Link href="#annanComponent">Expenses</Nav.Link>
+            <NavDropdown title="Settings" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Something</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Something else here
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
