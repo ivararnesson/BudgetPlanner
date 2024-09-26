9f1f2761933b0da4b4d578ee7c511026226ee3c8
Ivar Arnesson <ivar.arnesson@gmail.com>
2024-09-26 11:11:54 +0200

    Added sidebar and fixed the CustomNavbar.css component

--- BudgetPlanner.Web/src/App.jsx
+++ BudgetPlanner.Web/src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import TodoList from './TodoList'
+import Sidebar from './components/Sidebar'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CustomNavbar from './components/CustomNavbar'
 
 function App() {
   
   return (
-      <CustomNavbar />
+      <>
+        <CustomNavbar />
+        <Sidebar />
+      </>
   )
 }
 
--- BudgetPlanner.Web/src/components/CustomNavbar.jsx
+++ BudgetPlanner.Web/src/components/CustomNavbar.jsx
@@ -5,7 +5,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link } from 'react-router-dom';
-import "./CustomNavbar.css"
+import "./SideNavbar.css"
 import bpIcon from "../assets/BP-icon.png"
 
 export default function CustomNavbar() {
@@ -53,3 +53,7 @@ export default function CustomNavbar() {
     </Navbar>
   );
 }
+
+// npm install react-bootstrap bootstrap
+// npm install react-router-dom
+
