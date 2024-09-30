2fa9d10c41d9c26012007b1d3f8673e64cb3a327
Paul Hertz <paularam22@gmail.com>
2024-09-30 09:15:35 +0200

    clean

--- BudgetPlanner.Web/src/components/CustomNavbar.jsx
+++ BudgetPlanner.Web/src/components/CustomNavbar.jsx
@@ -3,7 +3,6 @@ import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link } from 'react-router-dom';
 import "./style/SideNavbar.css"
 import bpIcon from "../assets/BP-icon.png"
