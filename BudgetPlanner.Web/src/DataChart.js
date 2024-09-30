185090717080898fb1786e92d4a24c7ff4cf9d9e
Paul Hertz <paularam22@gmail.com>
2024-09-30 11:50:08 +0200

    name

--- BudgetPlanner.Web/src/DataChart.js
+++ BudgetPlanner.Web/src/DataChart.js
@@ -1,14 +1,59 @@
 
+let januariIncome = 31.235;
+let februariIncome = 33.195;
+let marsIncome = 33.753;
+let aprilIncome = 30.432;
+let majIncome = 29.324;
+let juniIncome = 32.643;
+let juliIncome = 33.853;
+let augustiIncome = 31.252;
+let septemberIncome = 32.637;
+let oktoberIncome = 33.324;
+let novemberIncome = 32.925;
+let decemberIncome = 31.321;
+
+let januariExpense = 19.233;
+let februariExpense = 15.762;
+let marsExpense = 17.291;
+let aprilExpense = 10.234;
+let majExpense = 15.932;
+let juniExpense = 16.373;
+let juliExpense = 14.643;
+let augustiExpense = 16.322;
+let septemberExpense = 15.873;
+let oktoberExpense = 16.325;
+let novemberExpense = 23.902;
+let decemberExpense = 18.932;
+
+
 const incomeData = [
-  31.235, 33.195, 33.753, 30.432, 29.324,
-  32.643, 33.853, 31.252, 32.637, 33.324,
-  32.925, 31.321,
+  januariIncome, 
+  februariIncome, 
+  marsIncome, 
+  aprilIncome, 
+  majIncome,
+  juniIncome, 
+  juliIncome, 
+  augustiIncome, 
+  septemberIncome, 
+  oktoberIncome,
+  novemberIncome, 
+  decemberIncome, 
 ];
 
 const expenseData = [
-  19.233, 15.762, 17.291, 10.234, 15.932,
-  16.373, 14.643, 16.322, 15.873, 16.325,
-  23.902, 18.932,
+  januariExpense, 
+  februariExpense, 
+  marsExpense, 
+  aprilExpense, 
+  majExpense,
+  juniExpense, 
+  juliExpense, 
+  augustiExpense, 
+  septemberExpense, 
+  oktoberExpense,
+  novemberExpense, 
+  decemberExpense,
 ];
 
 export { incomeData, expenseData };
