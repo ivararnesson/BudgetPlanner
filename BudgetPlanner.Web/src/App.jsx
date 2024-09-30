67b3edc71fe325eab6ef38d6b43548f5a3ef0fe5
Brissle <Erik.brissle@bredband2.com>
2024-09-30 13:02:47 +0200

    Merge Fix Conflicts

--- BudgetPlanner.Web/src/App.jsx
+++ BudgetPlanner.Web/src/App.jsx
@@ -8,34 +8,6 @@ import CustomFooter from "./Components/footer";
 import ExpensesPage from '../src/Components/ExpenceComponent';
 
 function App() {
-<<<<<<< HEAD
-    const [incomes, setIncomes] = useState([]);
-    const [totalIncome, setTotalIncome] = useState(0); 
-
-    useEffect(() => {
-        const fetchTotalIncome = async () => {
-            try {
-                const response = await fetch('https://localhost:7246/api/income/total');
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                const data = await response.json();
-                setTotalIncome(data.totalIncome); 
-            } catch (error) {
-                console.error('Error fetching total income:', error);
-            }
-        };
-
-        fetchTotalIncome();
-    }, []);
-
-    return (
-        <div>
-            <h1>Budget Planner</h1>
-            <IncomeComponent setIncomes={setIncomes} totalIncome={totalIncome} setTotalIncome={setTotalIncome} />
-            <Dashboard />
-        </div>
-=======
 
     return (
             <Router>
@@ -48,7 +20,6 @@ function App() {
                 </Routes>
       <CustomFooter />
             </Router>       
->>>>>>> 7530c60a25ff83c814bad3197fd0e5f8ad4b2703
     );
 }
 
